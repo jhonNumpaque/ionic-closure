52890ddef34404a85610396767feeee5929cc511
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-12-19 16:02:51 -0600

    add http

--- build_closure.sh
+++ build_closure.sh
@@ -19,8 +19,8 @@ OPTS=(
 
   node_modules/zone.js/dist/zone.js
   $(find vendor/rxjs -name *.js)
-  node_modules/@angular/{core,common,compiler,platform-browser}/index.js
-  $(find node_modules/@angular/{core,common,compiler,platform-browser}/src -name *.js)
+  node_modules/@angular/{core,common,compiler,http,platform-browser}/index.js
+  $(find node_modules/@angular/{core,common,compiler,http,platform-browser}/src -name *.js)
   "built/*.js"
   "--entry_point=./built/bootstrap"
 )
