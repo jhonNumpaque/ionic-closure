b8decaa6f4a87d279a18a8e5f94bb4c7febe3c38
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-01-19 15:26:42 -0800

    template enabling the next page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,23 +6,28 @@ import { NavController } from 'ionic-angular/es2015/index';
   selector: 'page-home',
   template: `
   <ion-header>
-    <ion-navbar>
-      <ion-title>
-        Ionic Blank
-      </ion-title>
-    </ion-navbar>
-  </ion-header>
+  <ion-navbar>
+    <ion-title>
+      Ionic Blank
+    </ion-title>
+  </ion-navbar>
+</ion-header>
 
-  <ion-content padding>
-    The world is your oyster.
-    <p>
-      If you get lost, the <a href="http://ionicframework.com/docs/v2">docs</a> will be your guide.
-    </p>
-  </ion-content>
+<ion-content padding>
+  The world is your oyster.
+  <p>
+    If you get lost, the <a href="http://ionicframework.com/docs/v2">docs</a> will be your guide.
+  </p>
+  <button ion-button (click)="nextPage()">Next Page</button>
+</ion-content>
   `
 })
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  }
+
+  nextPage() {
+    this.navCtrl.push('PageOne');
   }
 }
