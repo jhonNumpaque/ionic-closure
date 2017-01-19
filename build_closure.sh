0501d121bf1b210e0ad2039cee1efff9274e33f8
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-01-19 12:02:16 -0800

    removing bro, bro

--- build_closure.sh
+++ build_closure.sh
@@ -63,6 +63,6 @@ for script in dist/bundle.js node_modules/zone.js/dist/zone.min.js; do
   gzip --keep -f $script
   # requires brotli
   # on Mac: brew install brotli
-  bro --force --quality 10 --input $script --output $script.brotli
+  # bro --force --quality 10 --input $script --output $script.brotli
   ls -alH ${script}*
 done
